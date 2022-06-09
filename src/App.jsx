fd5e7c054c12a7ebbc305f68f31a31ff1a13c809
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-06-10 00:33:30 +0100

    Deleted some files to keep the initial app set up cleaned up a bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import "./App.module.scss";
 import Body from "./components/layout/body/Body";
 import Header from "./components/layout/header/head/Header";
 import Sidebar from "./components/layout/sidebar/side/Sidebar";
