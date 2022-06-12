534b63dbf51f51addc3350faa45e9ed8978be024
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-06-12 03:44:25 +0100

    added state to hold recipedata

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import axios from "axios";
 import Header from "./components/layout/header/head/Header";
 import Main from "./components/layout/main/Main";
 
 const App = () => {
+  const [recipeData, setRecipeData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const searchRecipes = async () => {
+    setLoading(true);
     const res = await axios.get(
       `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
     );
     const { recipe } = res.data.data;
-    console.log(res.data.data);
-    console.log(recipe);
+    setRecipeData(recipe);
+    console.log(recipeData);
   };
 
   searchRecipes();
